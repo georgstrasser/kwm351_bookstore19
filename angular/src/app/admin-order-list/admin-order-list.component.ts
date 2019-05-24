49b5cc36b53851cc74dad92fe83f3358e4fbf950
Georg Strasser <gstrasser1@gmx.net>
2019-05-24 11:02:14 +0200

    returned everything

--- angular/src/app/admin-order-list/admin-order-list.component.ts
+++ angular/src/app/admin-order-list/admin-order-list.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {Order} from "../shared/order";
 import {OrderService} from "../shared/order.service";
-import {Book} from "../shared/book";
 
 @Component({
-  selector: 'bs-admin-order-list',
-  templateUrl: './admin-order-list.component.html',
-  styles: []
+    selector: 'bs-admin-order-list',
+    templateUrl: './admin-order-list.component.html',
+    styles: []
 })
 export class AdminOrderListComponent implements OnInit {
 
     orders : Order[];
     constructor(
-        private os: OrderService
+        private os: OrderService,
     ) { }
 
     ngOnInit() {
@@ -20,18 +19,8 @@ export class AdminOrderListComponent implements OnInit {
         this.os.getAll().subscribe(
             res => {
                 this.orders = res;
-                for(let i=0; i<this.orders.length; i++){
-                    for(let j=0; j<this.orders[i].positions.length; j++){
-                        //creating a default book
-                        this.orders[i].positions[j].book = new Book(0,'','',[],new Date,0,0);
-                        this.os.getBookByID(this.orders[i].positions[j].book_id).subscribe(res => {
-                            this.orders[i].positions[j].book = res;
-                        });
-
-                    }
-                }
                 console.log(this.orders);
-        });
+            });
 
     }
 
